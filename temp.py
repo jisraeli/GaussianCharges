57cd2c573700a0dda80b19abe1d5f6d73cc36dd5
jisraeli <jisraeli@vspm15.stanford.edu>
2014-04-21 11:45:04 -0700

    switched to Reference

--- temp.py
+++ temp.py
@@ -10,7 +10,7 @@ import simulation1
 import sys
 
 pdb = app.PDBFile('TwoWaters.pdb')
-pdb.topology.setUnitCellDimensions((10,10,10))
+pdb.topology.setUnitCellDimensions((2,2,2))
 forcefield = app.ForceField('amber99sbildn.xml', 'tip3p.xml')
 
 system = forcefield.createSystem(pdb.topology, nonbondedMethod=app.PME, 
@@ -20,21 +20,20 @@ system = forcefield.createSystem(pdb.topology, nonbondedMethod=app.PME,
 integrator = mm.VerletIntegrator(2.0*unit.femtoseconds)
 integrator.setConstraintTolerance(0.00001)
 
-platform = mm.Platform.getPlatformByName('CUDA')
+platform = mm.Platform.getPlatformByName('Reference')
 simulation = app.Simulation(pdb.topology, system, integrator, platform)
 simulation.context.setPositions(pdb.positions)
 
 print('Minimizing...')
 simulation.minimizeEnergy()
-print "works so far",
 print simulation.context.getState(getEnergy=True).getPotentialEnergy()
 
 simulation.context.setVelocitiesToTemperature(300*unit.kelvin)
 print('Equilibrating...')
 simulation.step(100)
 
-simulation.reporters.append(app.DCDReporter('trajectory.dcd', 1000))
-simulation.reporters.append(app.StateDataReporter(stdout, 1000, step=True, 
+simulation.reporters.append(app.DCDReporter('CoulombTrajectory.dcd', 10))
+simulation.reporters.append(app.StateDataReporter(stdout, 10, step=True, 
     potentialEnergy=True, temperature=True, progress=True, remainingTime=True, 
     speed=True, totalSteps=1000, separator='\t'))
 
