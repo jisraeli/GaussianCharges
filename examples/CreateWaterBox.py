87906940d50a3f41014aadef929ced29417eaa83
jisraeli <jisraeli@stanford.edu>
2014-04-27 12:57:49 -0700

    moved pdb's

---
+++ examples/CreateWaterBox.py
@@ -0,0 +1,24 @@
+from pdbfixer.pdbfixer import PDBFixer
+from simtk.openmm.app import PDBFile
+from simtk.openmm import *
+from simtk.unit import *
+import optparse
+
+def parse_cmdln():
+        import os
+        parser=optparse.OptionParser()
+        parser.add_option('-p','--pdb', dest='pdb', type='string')
+        parser.add_option('-o','--out-file', dest='outfile', type='string', default='WaterBox.pdb')
+        (options, args) = parser.parse_args()
+        return (options, args)
+
+def run(options):
+        fixer = PDBFixer(options['pdb'])
+        fixer.addMissingHydrogens(7.0)
+        fixer.addSolvent(boxSize=Vec3(2,2,2)*nanometers, padding=None, 
+                    positiveIon='Na+', negativeIon='Cl-', ionicStrength=0.0*molar)
+        PDBFile.writeFile(fixer.topology, fixer.positions, open(options['outfile'], 'w'))
+
+if __name__=="__main__":
+        (options, args) = parse_cmdln()
+        run(vars(options))
