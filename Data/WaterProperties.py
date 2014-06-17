e888e2e8e389d5d82f5faf4bd94fd16dc4e18dce
jisraeli <jisraeli@stanford.edu>
2014-06-16 19:34:03 -0700

    need to finish plots

--- Data/WaterProperties.py
+++ Data/WaterProperties.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import*
+from matplotlib import pyplot as plt
 
 
 def getProperty(datalines, lineIndex):
@@ -7,12 +8,15 @@ def getProperty(datalines, lineIndex):
     for i in range(4):
         for elem in properties:
             if elem in datalines[lineIndex-i]:
+                print elem
                 return elem
 
 
 
 #read data into array
-lines = open('simple_liquidTest.out').readlines()
+
+#lines = open('simple_liquidTest.out').readlines()
+lines = open('tip3g_liquidTest.out').readlines()
 datalines = []
 for line in lines:
     dataline = line.split()
@@ -25,12 +29,14 @@ finalTemp = '298.15'
 reachedInit = False
 reachedFinal = False
 passedFinal = False
-data = []
+data = {}
+datablock = []
 count = 0
 for i in range(len(datalines)):
     if not reachedInit and initTemp in datalines[i]:
         reachedInit = True
         passedFinal = False
+        prop = getProperty(datalines, i)
     elif reachedInit and finalTemp in datalines[i]:
         reachedFinal = True
         passedFinal = False
@@ -38,11 +44,24 @@ for i in range(len(datalines)):
         passedFinal = True
         reachedInit = False
         reachedFinal = False
+        data[prop] = datablock
+        datablock = []
 
     if reachedInit and not passedFinal:
         datarow = []
-        for elem in dataline:
-            datarow.append(float(elem))
-        datablock.
+        for elem in datalines[i]:
+            if not elem in ['atm', '+-']:
+                datarow.append(float(elem))
+        datablock.append(datarow)
 
 
+#for prop in data.keys()[:1]:
+prop = 'Density'
+Data = np.asarray(data[prop])
+tempVals = Data[:, 0]
+experimentalVals = Data[:, 2]
+calcVals = Data[:, 3]
+calcDevs = Data[:, 4]
+plt.plot(tempVals, experimentalVals, color='red')
+plt.plot(tempVals, calcVals, color='blue')
+plt.show()
